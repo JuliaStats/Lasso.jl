f856c1be761e6aa081ffac48e8e049e89fa3d302
Asaf Manela <AsafManela@users.noreply.github.com>
2018-10-31 23:33:36 -0500

    slightly more tests

--- test/cross_validation.jl
+++ test/cross_validation.jl
@@ -12,21 +12,25 @@ path = fit(LassoPath, X, y; offset=offset)
 
 coefsAICc = coef(path;select=:AICc)
 segminAICc = minAICc(path)
+@test segminAICc == 71
 @test coefsAICc == β[:,segminAICc]
 
 Random.seed!(13)
 gen = Kfold(length(y),10)
 segCVmin = cross_validate_path(path;gen=gen)
 coefsCVmin = coef(path;select=:CVmin)
+@test segCVmin == 71
 @test coefsCVmin == β[:,segCVmin]
 
 Random.seed!(13)
 gen = Kfold(length(y),10)
 segCVmin = cross_validate_path(path,X,y; gen=gen, offset=offset)
 coefsCVmin = coef(path;select=:CVmin)
+@test segCVmin == 71
 @test coefsCVmin == β[:,segCVmin]
 
 Random.seed!(13)
 coefsCV1se = coef(path;select=:CV1se,nCVfolds=20)
 segCV1se = cross_validate_path(path,X,y;select=:CV1se,gen=gen,offset=offset)
+@test segCV1se == 42
 @test coefsCV1se == β[:,segCV1se]
