d200d879adf73ac2c01bb4a155aefe6b848e91ea
Asaf Manela <AsafManela@users.noreply.github.com>
2019-05-11 21:58:54 -0500

    create a copy of y for mu to avoid changing it later

--- src/Lasso.jl
+++ src/Lasso.jl
@@ -292,7 +292,7 @@ function build_model(X::AbstractMatrix{T}, y::FPVector, d::Normal, l::IdentityLi
                      lp::LinPred, λminratio::Real, λ::Union{Vector,Nothing},
                      wts::Union{FPVector,Nothing}, offset::Vector, α::Real, nλ::Int,
                      ω::Union{Vector,Nothing}, intercept::Bool, irls_tol::Real, dofit::Bool) where T
-    mu = isempty(offset) ? y : y + offset
+    mu = isempty(offset) ? copy(y) : y + offset
     nullmodel = LinearModel(LmResp{typeof(y)}(fill!(similar(y), 0), offset, wts, y),
                             GLM.cholpred(nullX(X, intercept, ω), false))
     fit!(nullmodel)
@@ -355,12 +355,12 @@ function standardizeX(X::AbstractMatrix{T}, standardize::Bool) where T
 end
 
 """
-    fit(LassoPath, X, y, d=Normal(), l=canonicallink(d); ...)   
-    
+    fit(LassoPath, X, y, d=Normal(), l=canonicallink(d); ...)
+
 fits a linear or generalized linear Lasso path given the design
 matrix `X` and response `y`:
 
-``\\underset{\\beta}{\\operatorname{argmin}} -\\frac{1}{N} \\mathcal{L}(y|X,\\beta) 
+``\\underset{\\beta}{\\operatorname{argmin}} -\\frac{1}{N} \\mathcal{L}(y|X,\\beta)
 + \\lambda\\left[(1-\\alpha)\\frac{1}{2}\\|\\beta\\|_2^2 + \\alpha\\|\\beta\\|_1\\right]``
 
 The optional argument `d` specifies the conditional distribution of
@@ -372,40 +372,40 @@ or ``\\mathcal{L}(y|X,\\beta) = -\\frac{1}{2}\\|y - X\\beta\\|_2^2 + C``
 # Examples
 ```julia
 fit(LassoPath, X, y)    # L1-regularized linear regression
-fit(LassoPath, X, y, Binomial(), Logit(); 
-    α=0.5) # Binomial logit regression with an Elastic net combination of 
+fit(LassoPath, X, y, Binomial(), Logit();
+    α=0.5) # Binomial logit regression with an Elastic net combination of
            # 0.5 L1 and 0.5 L2 regularization penalties
 ```
 # Arguments
-- `wts=ones(length(y))`: Weights for each observation                                    
-- `offset=zeros(length(y))`: Offset of each observation                                    
-- `λ`: can be used to specify a specific set of λ values at which models are fit. 
+- `wts=ones(length(y))`: Weights for each observation
+- `offset=zeros(length(y))`: Offset of each observation
+- `λ`: can be used to specify a specific set of λ values at which models are fit.
     If λ is unspecified, Lasso.jl selects nλ logarithmically spaced λ values from
     `λmax`, the smallest λ value yielding a null model, to
     `λminratio * λmax`.
 - `nλ=100` number of λ values to use
 - `λminratio=1e-4` if more observations than predictors otherwise 0.001.
-- `stopearly=true`: When `true`, if the proportion of deviance explained 
+- `stopearly=true`: When `true`, if the proportion of deviance explained
     exceeds 0.999 or the difference between the deviance explained by successive λ
-    values falls below `1e-5`, the path stops early.                                    
-- `standardize=true`: Whether to standardize predictors to unit standard deviation 
-    before fitting.                              
+    values falls below `1e-5`, the path stops early.
+- `standardize=true`: Whether to standardize predictors to unit standard deviation
+    before fitting.
 - `intercept=true`: Whether to fit an (unpenalized) model intercept.
-- `algorithm`: Algorithm to use. 
-    `NaiveCoordinateDescent` iteratively computes the dot product of the 
-    predictors with the  residuals, as opposed to the         
-    `CovarianceCoordinateDescent` algorithm, which uses a precomputed Gram matrix.                
-    `NaiveCoordinateDescent` is typically faster when there are many  
-    predictors that will not enter the model or when fitting        
-    generalized linear models.                                      
-    By default uses `NaiveCoordinateDescent` if more than 5x as many predictors 
+- `algorithm`: Algorithm to use.
+    `NaiveCoordinateDescent` iteratively computes the dot product of the
+    predictors with the  residuals, as opposed to the
+    `CovarianceCoordinateDescent` algorithm, which uses a precomputed Gram matrix.
+    `NaiveCoordinateDescent` is typically faster when there are many
+    predictors that will not enter the model or when fitting
+    generalized linear models.
+    By default uses `NaiveCoordinateDescent` if more than 5x as many predictors
     as observations or model is a GLM. `CovarianceCoordinateDescent` otherwise.
 - `randomize=true`: Whether to randomize the order in which coefficients are
     updated by coordinate descent. This can drastically speed
     convergence if coefficients are highly correlated.
-- `maxncoef=min(size(X, 2), 2*size(X, 1))`: maximum number of coefficients 
+- `maxncoef=min(size(X, 2), 2*size(X, 1))`: maximum number of coefficients
     allowed in the model. If exceeded, an error will be thrown.
-- `dofit=true`: Whether to fit the model upon construction. If `false`, the 
+- `dofit=true`: Whether to fit the model upon construction. If `false`, the
     model can be fit later by calling `fit!(model)`.
 - `cd_tol=1e-7`: The tolerance for coordinate descent iterations iterations in
     the inner loop.
@@ -422,7 +422,7 @@ fit(LassoPath, X, y, Binomial(), Logit();
     between successive iterations drops below the specified
     tolerance. This is the criterion used by GLM.jl.
 - `minStepFac=0.001`: The minimum step fraction for backtracking line search.
-- `penalty_factor=ones(size(X, 2))`: Separate penalty factor ``\\omega_j`` 
+- `penalty_factor=ones(size(X, 2))`: Separate penalty factor ``\\omega_j``
     for each coefficient ``j``, i.e. instead of ``\\lambda`` penalties become
     ``\\lambda\\omega_j``.
     Note the penalty factors are internally rescaled to sum to
